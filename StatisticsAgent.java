da3d28a71264440b1c37352cd41cb942ee12d3ac
umasshokie <mmolsen@MMOMac.local>
2014-06-23 12:20:11 -0400

    reverting to correct version

--- StatisticsAgent.java
+++ StatisticsAgent.java
@@ -1,5 +1,7 @@
 package sim.app.PVP_V2;
 
+import java.io.IOException;
+
 import sim.engine.SimState;
 import sim.engine.Steppable;
 
@@ -19,6 +21,8 @@ public class StatisticsAgent extends Animal implements Steppable {
 	public StatisticsAgent(double i)
 	{
 		interval = i;
+		//write("Time, numPrey, numPred, preyDeathByInterval, preyReproductionByInterval, predOutran, predDeathByInterval, predReproductionByInterval, preyCaughtByInterval, preyStay, predStay\n");
+		write("Time, numPrey, numPred, preyDeath, preyReproduction, predOutran, predDeath, predReproduction, preyCaught, preyStay, predStay\n");
 	}
 	@Override
 	public void step(SimState state) {
@@ -57,17 +61,18 @@ public class StatisticsAgent extends Animal implements Steppable {
 	*Input: SimState
 	*Output:Statistics to screen
 	**/
-	protected void printStats(SimState state){
+	protected void printStats(SimState state)
+	{
 	
-		write("\nTimeStep:" + (int)state.schedule.getTime() + ","); //added minus 1, as if it is scheduled as first agent then the stats are through prior timestep
+		write((int)state.schedule.getTime() + ","); //added minus 1, as if it is scheduled as first agent then the stats are through prior timestep
 		
-		double finalRepRatePrey = Prey.reproductionCollectPrey/interval;
-		double finalDeathRatePrey = Prey.deathCollectPrey/interval;
+		double finalRepRatePrey = Prey.reproductionCollectPrey;///(double)interval;
+		double finalDeathRatePrey = Prey.deathCollectPrey;///(double)interval;
 		
-		double finalRepRatePredator = Predator.reproductionCollectPredator/interval;
-		double finalDeathRatePredator = Predator.deathCollectPredator/interval;
-		double predOutranRate = Prey.predOutran/interval;
-		double preyCaughtRate = Predator.preyCaught/interval;
+		double finalRepRatePredator = Predator.reproductionCollectPredator;///(double)interval;
+		double finalDeathRatePredator = Predator.deathCollectPredator;///(double)interval;
+		double predOutranRate = Prey.predOutran;///(double)interval;
+		double preyCaughtRate = Predator.preyCaught;///(double)interval;
 		
 		/*
 		write("Prey,");
@@ -87,16 +92,25 @@ public class StatisticsAgent extends Animal implements Steppable {
 		write("Food Clustered" + "Yes" + ",");
 		*/
 		write(numPrey + ",");
+		write(numPredator + ",");
 		write(finalDeathRatePrey + ",");
 		write(finalRepRatePrey + ",");
 		write(predOutranRate + ",");
-		write(preyStay + ",");
-		write(numPredator + ",");
 		write(finalDeathRatePredator + ",");
 		write(finalRepRatePredator + ",");
 		write(preyCaughtRate + ",");
-		write(predStay + ",");
-		write(".1");
+		write(preyStay + ",");
+		write(predStay + "\n");
+		//write(".1" + ",");
+		//write(((PVP_2) state).getClusters()+"\n");
+		
+		try{
+		writer.flush();//make sure everything is written
+		}
+		catch(IOException e)
+		{
+			System.err.println("Could not flush log file");
+		}
 		
 		Prey.reproductionCollectPrey = 0;
 		Prey.deathCollectPrey = 0;
@@ -106,8 +120,7 @@ public class StatisticsAgent extends Animal implements Steppable {
 		Predator.preyCaught = 0;
 		preyStay = 0;
 		predStay = 0;
-
-		
+			
 	}
 	@Override
 	protected void eat(Object p, SimState state) {
